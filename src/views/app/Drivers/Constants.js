fd72f466443cd699e944aa37fe1ed437b2dfa17e
pankaj Vashisht <iampankajvashisht@gmail.com>
2020-01-01 19:34:25 +0530

    add new moduels

---
+++ src/views/app/Drivers/Constants.js
@@ -0,0 +1,40 @@
+
+export const quillModules = {
+    toolbar: [
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" }
+      ],
+      ["link", "image"],
+      ["clean"]
+    ]
+};
+
+export const initialState = {
+    name: '',
+    email: '',
+    password: '',
+    latitude: ''  || 0,
+    longitude: '' || 0,
+    address: '',
+    phone: '',
+    user_type: 3,
+    card_informations:null
+};
+
+export const quillFormats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image"
+  ];
