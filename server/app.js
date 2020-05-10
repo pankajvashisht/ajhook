c4041660dfdf26c6e56a8c4e651c51d726de062d
pankajvashisht-ucreate <pankaj@ucreate.co.in>
2020-05-10 12:13:27 +0530

    add new page

--- server/app.js
+++ server/app.js
@@ -7,9 +7,8 @@ const useragent = require('express-useragent');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 const process = require('process');
-const cors = require('cors')
+const cors = require('cors');
 global.appRoot = path.resolve(__dirname);
-
 
 const adminRouter = require('./routes/admin');
 const apiRouter = require('./routes/apis');
@@ -24,9 +23,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // file upload file
 app.use(
-  fileUpload({
-    createParentPath: true,
-  }),
+	fileUpload({
+		createParentPath: true,
+	})
 );
 
 // view engine setup
@@ -40,45 +39,43 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../build')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.get('/admin/*',(req, res) => {
-  res.sendFile(path.resolve(__dirname, '../build/index.html'));
-})
-
-app.use(function(req, res, next) {
-  global.appURL = req.protocol + '://' + req.get('host')+'/';
-  next();
+app.get('/admin/*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, '../build/index.html'));
 });
 
+app.use(function (req, res, next) {
+	global.appURL = req.protocol + '://' + req.get('host') + '/';
+	next();
+});
 
 app.use('/admins', adminRouter);
 app.use('/apis/v1/', apiRouter);
 app.use('/users', users);
-app.use('/welcome', index);
+app.use('/', index);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 process
-  .on('unhandledRejection', (reason, p) => {
-    console.error(reason, 'Unhandled Rejection at Promise', p);
-  })
-  .on('uncaughtException', (err) => {
-    console.error(err, 'Uncaught Exception thrown');
-    // process.exit(1);
-  });
+	.on('unhandledRejection', (reason, p) => {
+		console.error(reason, 'Unhandled Rejection at Promise', p);
+	})
+	.on('uncaughtException', (err) => {
+		console.error(err, 'Uncaught Exception thrown');
+		// process.exit(1);
+	});
 
 module.exports = app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/welcome', function (req, res) {
+	res.render('index', { title: 'Express' });
+});
+router.get('/support', function (req, res) {
+	res.render('support', { title: 'Express' });
 });
 
 module.exports = router;
